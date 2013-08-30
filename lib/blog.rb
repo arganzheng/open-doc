aebc7240288dfc8e51caedd710c26d4fc63c347c
arganzheng <arganzheng@gmail.com>
2013-08-30 16:40:43 +0800

    单个文件显示：根据URL映射到文件目录

--- lib/blog.rb
+++ lib/blog.rb
@@ -2,26 +2,63 @@ require 'sinatra/base'
 require 'ostruct'
 require 'yaml'
 require 'time'
+require "logger"
 
 class Blog < Sinatra::Base
   set :root, File.expand_path('../../', __FILE__)
-  set :articles, []
+  set :markdown, :layout_engine => :erb
   set :app_file, __FILE__
+  set :articles, []
+
+  configure do
+    enable :logging
+    file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+    file.sync = true
+    use Rack::CommonLogger, file
+  end
+
+  before do
+    logger.datetime_format = "%Y/%m/%d @ %H:%M:%S "
+    logger.level = Logger::INFO
+  end
+
+  
   Dir.glob "#{root}/articles/**/*.md" do |file|
     meta, content   = File.read(file, :encoding => "utf-8").split("\n\n", 2)
     article         = OpenStruct.new YAML.load(meta)
     article.date    = Time.parse article.date.to_s
     article.content = content
     article.slug    = File.basename(file, '.md')
-    get "/doc/#{article.slug}" do
-      erb :article, :locals => { :article => article }, :layout => :post
-    end
+    #get "/doc/#{article.slug}" do
+    #  erb :article, :locals => { :article => article }, :layout => :post
+    #end
     articles << article
   end
   articles.sort_by! { |article| article.date }
   articles.reverse!
    
   get '/' do
-    erb :index
+    erb :index, :layout => :'layout/layout'
   end
+
+  get %r{/doc/([\w[_/-]?]+)?[/]?} do | page|
+    if(page.end_with?("/"))
+      page = page.chop;
+    end
+
+    file = File.join(settings.root, "articles", page + ".md")
+    if(File.exists?(file))
+      meta, content   = File.read(file, :encoding => "utf-8").split("\n\n", 2)
+      article         = OpenStruct.new YAML.load(meta)
+      article.date    = Time.parse article.date.to_s
+      article.content = content
+      article.slug    = File.basename(file, '.md')
+  
+      erb :article, :locals => { :article => article }, :layout => :'layout/post'
+    else
+      logger.error "404" + file
+      raise error(404)
+    end
+  end
+
 end
